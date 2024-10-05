4fbe453771dfaed9e0ff35ca6c33d73e78fde309
Jarpekulo <j.sykuteraaa@gmail.com>
2024-10-05 18:20:35 +0200

    lgin

--- login.php
+++ login.php
@@ -1,1 +1,17 @@
-#prosze nie oceniac poprzednij commitow to bylo tylko dal testow pozdrawiamy
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ToZrob - Login</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="login-container">
+        <div class="login-box">
+            <h1>ToZrob</h1>
+
+        </div>
+    </div>
+</body>
+</html> 
